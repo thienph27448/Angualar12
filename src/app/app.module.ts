42fc11b74fe0d95c5d52ba10e20f17bb4aa511c2
thienddph27448 <thienddph27448@fpt.edu.vn>
2022-11-17 19:46:23 +0700

    module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,15 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AboutComponent } from './about/about.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
-  ],
+    AppComponent,
+    AboutComponent,
+
+   ],
   imports: [
     BrowserModule,
     AppRoutingModule
